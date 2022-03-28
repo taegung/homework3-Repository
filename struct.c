ea2a2a7657e9fe3d887b3f6fecfc6d995ce104e2
태경 <63376555+taegung@users.noreply.github.com>
2022-03-28 18:20:28 +0900

    Add files via upload

---
+++ struct.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include<stdlib.h>
+struct student1 {//student1 구조체 정의//typedef로 구조체를 정의를 안해서 선언할때 할때 struct 사용
+char lastName;
+int studentId;
+char grade;
+};
+typedef struct {//student2 구조체 정의
+char lastName;
+int studentId;
+char grade;
+} student2;
+int main() {
+printf("[----- [윤태경] [2019038059] -----]\n");
+struct student1 st1 = {'A', 100, 'A'};//st1이라는 구조체 선언하고 lastName에 A studentId에 100 grade에 A 입력
+printf("st1.lastName = %c\n", st1.lastName);//st1 lastname 출력
+printf("st1.studentId = %d\n", st1.studentId);//st1 studentId 출력
+printf("st1.grade = %c\n", st1.grade);//st1 grade 출력
+student2 st2 = {'B', 200, 'B'};//st2이라는 구조체 선언하고 lastName에 B studentId에 200 grade에 B 입력
+printf("\nst2.lastName = %c\n", st2.lastName);//st2 lastname 출력
+printf("st2.studentId = %d\n", st2.studentId);//st2 studentId 출력
+printf("st2.grade = %c\n", st2.grade);//st2 grade 출력
+student2 st3;//st3이라는 구조체 선언
+st3 = st2;//st3 구조체에 st2구조체 값들 입력
+printf("\nst3.lastName = %c\n", st3.lastName);//st3 lastname 출력
+printf("st3.studentId = %d\n", st3.studentId);//st3 studentId 출력
+printf("st3.grade = %c\n", st3.grade);//st3 grade 출력
+
+/* equality test */
+if(st3.lastName!=st2.lastName)//st3.lastName와st2.lastName비교
+   printf("st3와 st2는 다르다");
+else if(st3.studentId!=st2.studentId)//st3.studentId와st2.studentId 비교
+    printf("st3와 st2는 다르다");
+else if(st3.grade!=st2.grade)//st3.grade와st2.grade 비교
+    printf("st3와 st2는 다르다");
+else
+   printf("st3와 st2는 같다");
+}
